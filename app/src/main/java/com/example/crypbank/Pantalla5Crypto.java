6cf8d5baf8a57cc7cc15f2222fe566cf4da30933
Andres Cuadrado <andres.cuadrado.collado@gmail.com>
2022-11-24 19:55:29 +0100

    Revisar codigo pantalla 5 Controlar errores pantalla 5 Modificar menu navegacion

--- app/src/main/java/com/example/crypbank/Pantalla5Crypto.java
+++ app/src/main/java/com/example/crypbank/Pantalla5Crypto.java
@@ -14,18 +14,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.crypbank.coingecko.models.Coin;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Pantalla5Crypto extends AppCompatActivity {
 
@@ -51,17 +47,15 @@ public class Pantalla5Crypto extends AppCompatActivity {
 
         Coin coin = (Coin) getIntent().getSerializableExtra("Coin");
         saldo = findViewById(R.id.editBalanceFive);
-        nombreCrypto = findViewById(R.id.editCrypto);
-        precioCrypto = findViewById(R.id.editpriceCrypto);
-        cantidad = findViewById(R.id.editTransferQuantity);
-        precioTotal = findViewById(R.id.editTotalAmount);
+        nombreCrypto = findViewById(R.id.editCryptoFive);
+        precioCrypto = findViewById(R.id.editpriceCryptoFive);
+        cantidad = findViewById(R.id.editTransferQuantityFive);
+        precioTotal = findViewById(R.id.editTotalAmountFive);
 
         botonComprar = findViewById(R.id.confirmBuyButton);
 
         myAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        balanceInformation();
 
         nombreCrypto.setText(coin.getNombre());
         precioCrypto.setText(String.valueOf(coin.getPrecio()));
@@ -70,6 +64,8 @@ public class Pantalla5Crypto extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        balanceInformation();
 
         cantidad.addTextChangedListener(new TextWatcher() {
             @Override
@@ -84,23 +80,36 @@ public class Pantalla5Crypto extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                double num1 = Double.parseDouble(precioCrypto.getText().toString());
-                double num2 = Double.parseDouble(cantidad.getText().toString());
-                operacion = num1 * num2;
+                try {
+                    double num1 = Double.parseDouble(precioCrypto.getText().toString());
+                    double num2 = Double.parseDouble(cantidad.getText().toString());
+                    operacion = num1 * num2;
 
-                precioTotal.setText(Double.toString(operacion));
+                    precioTotal.setText(Double.toString(operacion));
+                } catch(NumberFormatException numberFormatException) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error, asegurate de que el campo cantidad no está vacío.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         botonComprar.setOnClickListener(view -> {
-            balance = Double.parseDouble(saldo.getText().toString()) - Double.parseDouble(precioTotal.getText().toString());
-            actualizarFirebase();
+            try {
+                balance = Double.parseDouble(saldo.getText().toString()) - Double.parseDouble(precioTotal.getText().toString());
+                updateFirebase();
+            } catch(NumberFormatException numberFormatException) {
+                Toast.makeText(getApplicationContext(),
+                        "Error, asegurate de que el campo cantidad no está vacío.",
+                        Toast.LENGTH_LONG).show();
+            }
         });
-        BottomNavigationView menu = findViewById(R.id.navigationMenuFive);
-        menu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        NavigationBarView menu = findViewById(R.id.navigationMenuFive);
+        menu.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private void actualizarFirebase() {
+    private void updateFirebase() {
         String id = myAuth.getCurrentUser().getUid();
         mDatabase.child("Usuarios").child(id).child("Saldo").setValue(balance);
         Intent pantalla3 = new Intent(Pantalla5Crypto.this, Pantalla3Principal.class);
@@ -110,7 +119,6 @@ public class Pantalla5Crypto extends AppCompatActivity {
 
     public void balanceInformation() {
         String id= myAuth.getCurrentUser().getUid();
-
         mDatabase.child("Usuarios").child(id).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -119,32 +127,41 @@ public class Pantalla5Crypto extends AppCompatActivity {
                     saldo.setText(balanceProv);
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(getApplicationContext(), "Error de conexión con la BBDD.",
+                        Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private final NavigationBarView.OnItemSelectedListener mOnNavigationItemSelectedListener =
+            new NavigationBarView.OnItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.item1:
-                    Intent pantalla4 = new Intent(Pantalla5Crypto.this, Pantalla4Cuenta.class);
-                    startActivity(pantalla4);
+                    Intent pantalla3 = new Intent(Pantalla5Crypto.this,
+                            Pantalla3Principal.class);
+                    startActivity(pantalla3);
                     return true;
                 case R.id.item2:
-                    Intent pantalla5 = new Intent(Pantalla5Crypto.this, Pantalla5Crypto.class);
-                    startActivity(pantalla5);
+                    Intent pantalla4 = new Intent(Pantalla5Crypto.this,
+                            Pantalla4Cuenta.class);
+                    startActivity(pantalla4);
                     return true;
                 case R.id.item3:
-                    Intent pantalla6 = new Intent(Pantalla5Crypto.this, Pantalla6Perfil.class);
+                    Intent pantalla6 = new Intent(Pantalla5Crypto.this,
+                            Pantalla6Perfil.class);
                     startActivity(pantalla6);
                     return true;
                 case R.id.item4:
                     myAuth.signOut();
-                    Toast.makeText(getApplicationContext(), "Sesión cerrada.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),
+                            "Sesión cerrada.",
+                            Toast.LENGTH_SHORT
+                    ).show();
                     startActivity(new Intent(Pantalla5Crypto.this, Pantalla1Inicio.class));
                     finish();
                     return true;
